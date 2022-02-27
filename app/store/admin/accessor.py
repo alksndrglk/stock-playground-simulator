d02e318aa3f34ac58205281bb0cc34bb80fb03d4
aglk <alksndrglk@gmail.com>
2022-02-27 11:48:28 +0300

    Init Commit

---
+++ app/store/admin/accessor.py
@@ -0,0 +1,29 @@
+import typing
+from hashlib import sha256
+from typing import Optional
+
+from app.base.base_accessor import BaseAccessor
+from app.admin.models import Admin, AdminModel
+
+if typing.TYPE_CHECKING:
+    from app.web.app import Application
+
+
+class AdminAccessor(BaseAccessor):
+    async def connect(self, app: "Application"):
+        await super().connect(app)
+        await self.create_admin(
+            email=app.config.admin.email, password=app.config.admin.password
+        )
+
+    async def get_by_email(self, email: str) -> Optional[Admin]:
+        res = await AdminModel.query.where(AdminModel.email == email).gino.first()
+        if not res:
+            return None
+        return Admin(id=res.id, email=res.email, password=res.password)
+
+    async def create_admin(self, email: str, password: str) -> Optional[Admin]:
+        if not (await self.get_by_email(email)):
+            await AdminModel(
+                email=email, password=sha256(password.encode()).hexdigest()
+            ).create()
---
+++ app/store/database/database.py
@@ -0,0 +1,38 @@
+import gino
+from gino.api import Gino
+from app.base.base_accessor import BaseAccessor
+from app.store.database.gino import db
+from app.admin.models import *
+from app.quiz.models import *
+from sqlalchemy.engine.url import URL
+
+
+class Database:
+    db: Gino
+
+    def __init__(self, app: "Application"):
+        self.app = app
+        self.db: Optional[Gino] = None
+
+    async def connect(self, app: "Application", *_, **kw):
+        self._engine = await gino.create_engine(
+            URL(
+                drivername="asyncpg",
+                host=app.config.database.host,
+                database=app.config.database.database,
+                username=app.config.database.user,
+                password=app.config.database.password,
+                port=app.config.database.port,
+            ),
+            min_size=1,
+            max_size=1,
+        )
+        db.bind = self._engine
+        self.db = db
+
+    async def disconnect(self, *_, **kw):
+        pass
+        # self.app = None
+        # engine, db.bind = db.bind, None
+        # await engine.close()
+
