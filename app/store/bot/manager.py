e71c433cbd74bcb9349c18ea939a557cf5176f27
Aleksandr Goliak <alksndrglk@gmail.com>
2022-03-02 10:16:55 +0300

    bot/manager: Added start method

--- app/store/bot/manager.py
+++ app/store/bot/manager.py
@@ -18,14 +18,22 @@ class BotManager:
         for update in updates:
             if update.object.action.get("type") == "chat_invite_user":
                 await self.send_greeting(update.object.peer_id)
-            else:
-                print(update.object.action)
+            elif update.type == "message_event":
+                payload = update.object.payload.get("command")
+                if payload == "start":
+                    await self.start(update.object.peer_id)
                 await self.app.store.vk_api.send_message(
                     Message(
                         peer_id=update.object.peer_id,
-                        text="Для не корректной работы бота, дайте ему права Администратора в настройках",
+                        text="Для не корректной работы бота",
                     )
                 )
+
+    async def start(self, peer_id):
+        players = await self.app.store.vk_api.get_conversation_members(peer_id)
+        print(f"{players=}")
+        self.logger.info(players)
+
 
     async def send_greeting(self, peer_id):
         await self.app.store.vk_api.send_message(
@@ -34,13 +42,13 @@ class BotManager:
                 text="Для корректной работы бота, дайте ему права Администратора в настройках",
                 keyboard=json.dumps(
                     {
-                        "one_time": True,
+                        "inline": True,
                         "buttons": [
                             [
                                 {
                                     "action": {
-                                        "type": "text",
-                                        "payload": '{"button": "2"}',
+                                        "type": "callback",
+                                        "payload": '{"command": "start"}',
                                         "label": "Старт",
                                     },
                                     "color": "positive",
