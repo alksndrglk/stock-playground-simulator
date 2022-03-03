7f4fbd0a127658c9d2f060eb1ef3958c1e0a9fd2
Aleksandr Goliak <alksndrglk@gmail.com>
2022-03-03 16:33:03 +0300

    Full game initialization in db

--- app/store/stock/accessor.py
+++ app/store/stock/accessor.py
@@ -1,62 +1,76 @@
-from typing import Optional
-
+from typing import List
 from app.base.base_accessor import BaseAccessor
+
 from app.stock.models import (
-    Game,
+    BrokerageAccountModel,
     GameModel,
     GameXUser,
-    User,
-    Stock,
-    StockMarketEvent,
-    BrokerageAccount,
     UserModel,
     GameStockModel,
     StockModel,
 )
-from typing import List
+from app.store.database.gino import db
+from app.store.bot.dataclassess import VkUser
+from sqlalchemy.dialects.postgresql import insert
 
 
 class ExchangeAccessor(BaseAccessor):
-    async def create_game(self, players, peer_id):
-        game = await GameModel(chat_id=peer_id).create()
-        users = await UserModel.insert().gino.all(
-            [
-                {
-                    "user_id": player.user_id,
-                    "user_name": player.user_name,
-                }
-                for player in players
-            ]
+    async def create_game(self, players: List[VkUser], peer_id: int):
+        async with db.transaction():
+            game = await GameModel(chat_id=peer_id).create()
+            await self.user_registration(players, game.id)
+            await self.stocks_initialization(game.id)
+
+    async def user_registration(self, players: List[VkUser], game_id: int):
+        users = await (
+            insert(UserModel)
+            .values(
+                [
+                    {
+                        "user_id": player.vk_id,
+                        "user_name": player.user_name,
+                    }
+                    for player in players
+                ]
+            )
+            .on_conflict_do_nothing()
+            .returning(*UserModel)
+            .gino.all()
         )
+
         await GameXUser.insert().gino.all(
             [
                 {
-                    "game_id": game.id,
+                    "game_id": game_id,
                     "user_id": u.id,
                 }
                 for u in users
             ]
         )
+        await self.portfolio_creation(users, game_id)
 
-    async def get_stock_by_symbol(self, symbol: str) -> Optional[Stock]:
-        pass
+    async def portfolio_creation(self, users: list, game_id: int):
+        await BrokerageAccountModel.insert().gino.all(
+            [
+                {
+                    "user_id": u.id,
+                    "game_id": game_id,
+                    "points": 1000,
+                }
+                for u in users
+            ]
+        )
 
-    async def add_stock(
-        self, symbol: str, description: str, cost: float
-    ) -> Optional[Stock]:
-        pass
-
-    async def list_stocks(self) -> List[Stock]:
-        pass
-
-    async def list_events(self) -> List[StockMarketEvent]:
-        pass
-
-    async def create_user(self, user_name: str, vk_id: int) -> Optional[User]:
-        pass
-
-    async def get_user_stats_by_id(self, vk_id: int) -> Optional[BrokerageAccount]:
-        pass
-
-    async def get_game_stats_by_id(self, chat_id: int) -> Optional[Game]:
-        pass
+    async def stocks_initialization(self, game_id: int):
+        stocks = await StockModel.query.gino.all()
+        await GameStockModel.insert().gino.all(
+            [
+                {
+                    "symbol": stock.symbol,
+                    "description": stock.description,
+                    "cost": stock.cost,
+                    "game_id": game_id,
+                }
+                for stock in stocks
+            ]
+        )
