1ed1748c4d67beb7ae0c127c7fa6e3a000f64e50
aglk <alksndrglk@gmail.com>
2022-03-05 22:08:30 +0300

    Added message processing, errors, conditions

--- app/store/bot/errors.py
+++ app/store/bot/errors.py
@@ -1,2 +1,16 @@
 class RequestDoesNotMeetTheStandart(Exception):
-    pass
+    def __init__(self, message="Не понимаю сообщение."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class SymbolNotInPortfolio(Exception):
+    def __init__(self, message="Такой акции в портфеле нет."):
+        self.message = message
+        super().__init__(self.message)
+
+
+class OperationIsUnavailable(Exception):
+    def __init__(self, message="Не достаточно средств. Операция недоступна."):
+        self.message = message
+        super().__init__(self.message)
