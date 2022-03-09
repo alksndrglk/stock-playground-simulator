adff1aa8885f1b35f0395051d6a541afb451127d
Aleksandr Goliak <alksndrglk@gmail.com>
2022-03-09 16:49:08 +0300

    Typings.

--- app/stock/models.py
+++ app/stock/models.py
@@ -11,11 +11,65 @@ from app.store.bot.errors import SymbolNotInPortfolio, OperationIsUnavailable
 
 
 @dataclass
+class BrokerageAccount:
+    id: Optional[int] = None
+    user_id: int = 0
+    game_id: int = 0
+    points: float = 0
+    portfolio: Dict[str, int] = field(default_factory=dict)
+
+    def __str__(self) -> str:
+        return f"На счете: {self.points}$, акции: {self.portfolio}"
+
+    def sell(
+        self, symbol: str, quantity: int, cost: float
+    ) -> Optional["BrokerageAccount"]:
+        if symbol not in self.portfolio:
+            raise SymbolNotInPortfolio
+        if quantity > self.portfolio[symbol]:
+            raise OperationIsUnavailable
+
+        self.portfolio[symbol] -= quantity
+        if self.portfolio[symbol] == 0:
+            del self.portfolio[symbol]
+        self.points += quantity * cost
+        return self
+
+    def buy(
+        self, symbol: str, quantity: int, cost: float
+    ) -> Optional["BrokerageAccount"]:
+        if (quantity * cost) > self.points:
+            raise OperationIsUnavailable
+        self.portfolio[symbol] = self.portfolio.get(symbol, 0) + quantity
+        self.points -= quantity * cost
+        return self
+
+
+class BrokerageAccountModel(db.Model):
+    __tablename__ = "brokerage_accounts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("player.id", ondelete="CASCADE"), nullable=False
+    )
+    game_id = db.Column(
+        db.Integer, db.ForeignKey("game.id", ondelete="CASCADE"), nullable=False
+    )
+    points = db.Column(db.Float)
+    portfolio = db.Column(JSONB, server_default="{}")
+
+    def to_dct(self) -> BrokerageAccount:
+        return BrokerageAccount(**self.to_dict())
+
+
+@dataclass
 class User:
     id: Optional[int]
     user_id: int
     user_name: str
-    brokerage_account: Optional["BrokerageAccount"] = field(default_factory=dict)
+    brokerage_account: Optional["BrokerageAccount"] = field(
+        default_factory=BrokerageAccount
+    )
 
     def __str__(self) -> str:
         return str(self.brokerage_account)
@@ -158,11 +212,11 @@ class GameStockModel(db.Model, model("stock_in_game")):
         )
 
     @classmethod
-    def bulk_upsert(cls, stocks, diff, game_id):
+    def bulk_upsert(cls, stocks: List[Stock], diff: float):
         qs = insert(cls.__table__).values(
             [
                 {
-                    "cost": s.cost * (100 + diff) / 100,
+                    "cost": s.cost * diff,
                     "symbol": s.symbol,
                     "game_id": s.game_id,
                     "description": s.description,
@@ -174,9 +228,7 @@ class GameStockModel(db.Model, model("stock_in_game")):
         return (
             qs.on_conflict_do_update(
                 index_elements=[cls.symbol],
-                set_={
-                    "cost": qs.excluded["cost"]
-                },  # or even qs.excluded['some_column']
+                set_={"cost": qs.excluded["cost"]},
             )
             .returning(GameStockModel.__table__)
             .gino.all()
@@ -198,59 +250,7 @@ class StockMarketEventModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String, nullable=False)
-    diff = db.Column(db.Integer, nullable=False)
+    diff = db.Column(db.Float, nullable=False)
 
     def to_dct(self) -> StockMarketEvent:
         return StockMarketEvent(**self.to_dict())
-
-
-@dataclass
-class BrokerageAccount:
-    id: Optional[int]
-    user_id: int
-    game_id: int
-    points: float
-    portfolio: Dict[str, int]
-
-    def __str__(self) -> str:
-        return f"На счете: {self.points}$, акции: {self.portfolio}"
-
-    def sell(
-        self, symbol: str, quantity: int, cost: float
-    ) -> Optional["BrokerageAccount"]:
-        if symbol not in self.portfolio:
-            raise SymbolNotInPortfolio
-        if quantity > self.portfolio[symbol]:
-            raise OperationIsUnavailable
-
-        self.portfolio[symbol] -= quantity
-        if self.portfolio[symbol] == 0:
-            del self.portfolio[symbol]
-        self.points += quantity * cost
-        return self
-
-    def buy(
-        self, symbol: str, quantity: int, cost: float
-    ) -> Optional["BrokerageAccount"]:
-        if (quantity * cost) > self.points:
-            raise OperationIsUnavailable
-        self.portfolio[symbol] = self.portfolio.get(symbol, 0) + quantity
-        self.points -= quantity * cost
-        return self
-
-
-class BrokerageAccountModel(db.Model):
-    __tablename__ = "brokerage_accounts"
-
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(
-        db.Integer, db.ForeignKey("player.id", ondelete="CASCADE"), nullable=False
-    )
-    game_id = db.Column(
-        db.Integer, db.ForeignKey("game.id", ondelete="CASCADE"), nullable=False
-    )
-    points = db.Column(db.Float)
-    portfolio = db.Column(JSONB, server_default="{}")
-
-    def to_dct(self) -> BrokerageAccount:
-        return BrokerageAccount(**self.to_dict())
